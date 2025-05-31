b1a8c42efa0fa448eeee364730cc86172fd2d5c6
LineSpark4 <147136575+Armanlko-dev@users.noreply.github.com>
2025-05-31 21:27:49 +0530

    updated

--- lib/serivce/background_service/background_service.dart
+++ lib/serivce/background_service/background_service.dart
@@ -1,15 +1,10 @@
 import 'dart:async';
 import 'dart:ui';
 
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_background_service/flutter_background_service.dart';
-import 'package:flutter_background_service_android/flutter_background_service_android.dart';
-import 'package:flutter_background_service_ios/flutter_background_service_ios.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:travelx_driver/flavors.dart';
 import 'package:travelx_driver/home/bloc/home_cubit.dart';
-import 'package:travelx_driver/flavors.dart';
 
 Future<void> initializeBackgroundService() async {
   final service = FlutterBackgroundService();
